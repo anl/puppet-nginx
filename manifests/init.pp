ec129994c2853f533253c066bc54a36d4fba3425
Andrew Leonard <andy@hurricane-ridge.com>
2012-11-15 20:21:21 -0800

    Initial commit; boilerplate.

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# == Class: nginx
+#
+# Module to deploy Nginx.
+#
+# Supports Ubuntu 12.04.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Examples
+#
+#  include nginx
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard, Seattle Biomedical Research Institute
+#
+class nginx {
+
+
+}
